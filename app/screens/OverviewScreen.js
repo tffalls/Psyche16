37aff96a07cd2db7508afa504ef693673de647f2
Tarryn Falls <tffalls@asu.edu>
2019-01-21 21:58:12 -0700

    Asteroid page basic

--- app/screens/OverviewScreen.js
+++ app/screens/OverviewScreen.js
@@ -26,7 +26,8 @@ import {
 } from 'native-base';
 
 import headerStyle from '../styles/SideMenu.style';
-import {Fonts} from '../components/Fonts'
+import infoPageStyle from '../styles/InfoPage.style';
+import {Fonts} from '../components/Fonts';
 
 class OverviewScreen extends Component {
     render() {
@@ -45,34 +46,34 @@ class OverviewScreen extends Component {
                     </Body>
                     <Right style={{flex:1}} />
                 </Header>
-                <Content contentContainerStyle={styles.content} style={styles.pageStyle}>
-                    <Text style={styles.pageHeadingText}>Mission Overview</Text>
-                    <Text style={styles.pageBodyText}>
+                <Content contentContainerStyle={infoPageStyle.content} style={infoPageStyle.pageStyle}>
+                    <Text style={infoPageStyle.pageHeadingText}>Mission Overview</Text>
+                    <Text style={infoPageStyle.pageBodyText}>
                         Psyche is both the name of an asteroid orbiting the Sun between Mars and Jupiter 
                         — and the name of a NASA space mission to visit that asteroid, led by Arizona 
                         State University. The mission was chosen by NASA on January 4, 2017 as one of two 
                         missions for the agency’s Discovery Program, a series of relatively low-cost missions 
                         to solar system targets.
                     </Text>
-                    <Text style={styles.pageHeadingText}>Trajectory</Text>
-                    <Text style={styles.pageBodyText}>
+                    <Text style={infoPageStyle.pageHeadingText}>Trajectory</Text>
+                    <Text style={infoPageStyle.pageBodyText}>
                         The Psyche spacecraft is targeted to launch in summer 2022 and travel to the asteroid 
                         using solar-electric (low-thrust) propulsion, arriving in 2026, following a Mars flyby 
                         and gravity-assist in 2023. After arrival, the mission plan calls for 21 months spent 
                         at the asteroid, mapping it and studying its properties.
                     </Text>
-                    <Text style={styles.pageHeadingText}> Orbit</Text>
-                    <Text style={styles.pageBodyText}>
+                    <Text style={infoPageStyle.pageHeadingText}> Orbit</Text>
+                    <Text style={infoPageStyle.pageBodyText}>
                         Once the spacecraft arrives at the asteroid in 2026, plans call for it to spend 21 months 
                         at the asteroid, performing science operations from four staging orbits, which become successively closer.
                     </Text>
-                    <Card style={styles.cardStyle}>
-                        <CardItem style={styles.cardHeader}>
+                    <Card style={infoPageStyle.cardStyle}>
+                        <CardItem style={infoPageStyle.cardHeader}>
                             <Body>
-                                <Text style={styles.cardText}>Journey to a Metal World</Text>
+                                <Text style={infoPageStyle.cardText}>Journey to a Metal World</Text>
                             </Body>
                         </CardItem>
-                        <CardItem cardbody style={styles.cardBody}>
+                        <CardItem cardbody style={infoPageStyle.cardBody}>
                             <WebView
                                 javaScriptEnabled={true}
                                 domStorageEnabled={true}
@@ -87,53 +88,4 @@ class OverviewScreen extends Component {
     }
 }
 
-const styles = StyleSheet.create({
-    pageHeadingText: {
-        fontFamily: Fonts.RobotoBold,
-        fontSize: 32,
-        paddingTop: '5%',
-        paddingBottom: '1%', // prevent cuttoff letters
-        color: 'white',
-        width: '80%',
-    },
-    pageBodyText: {
-        fontFamily: Fonts.RobotoLight,
-        fontSize: 20,
-        padding: '10%',
-        color: 'white',
-    },
-    // titleLeftStyle: {
-    //     width: '80%',
-    // },
-    content: {
-        backgroundColor: '#140025',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    pageStyle: {
-        backgroundColor: '#140025', // may replace w/ background image from GD students?
-    },
-    cardStyle: {
-        backgroundColor: '#f79f27ff',
-        padding: '5%',
-        borderRadius: 10,
-        borderColor: '#f79f27ff',
-        width: '90%',
-    },
-    cardHeader: {
-        backgroundColor: '#f79f27ff',
-    },
-    cardBody: {
-        backgroundColor: '#f79f27ff',
-        height: 200,
-        width: '100%',
-        flex: 1,
-    },
-    cardText: {
-        fontFamily: Fonts.RobotoBold,
-        fontSize: 18,
-        color: '#140025',
-    }
-
-  });
 export default OverviewScreen;
